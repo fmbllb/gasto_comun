e3df66a681f3b8f2278fe68190daf2d46effb533
Fabian Mella Barra <fa.mella@duocuc.cl>
2024-11-10 19:36:55 -0300

    Update bill controller

---
+++ controllers/BillsController.py
@@ -0,0 +1,16 @@
+from services.BillsService import BillService
+
+class BillController:    
+    @staticmethod
+    def create_bill_controller(id_gasto, nom_gasto, 
+        total_gasto, fecha_gasto, tipo_gasto):
+        return BillService.create_bill(id_gasto, nom_gasto, 
+            total_gasto, fecha_gasto, tipo_gasto)
+            
+    @staticmethod
+    def get_bill_controller():        
+        return BillService.get_all_bills()
+    
+    @staticmethod
+    def get_bill_by_id_controller(id_gasto):
+        return BillService.get_bill_by_id(id_gasto)
