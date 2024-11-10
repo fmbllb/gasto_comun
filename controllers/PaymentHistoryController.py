626628fb7536d71c86c9c826f2d30d0b0a74f419
Fabian Mella Barra <fa.mella@duocuc.cl>
2024-11-10 19:45:39 -0300

    Create payment history

---
+++ controllers/PaymentHistoryController.py
@@ -0,0 +1,16 @@
+from services.PaymentHistoryService import PaymentHistoryService
+
+class PaymentHistoryController:    
+    @staticmethod
+    def create_payment_history_controller(idDepartamento, idGasto, 
+                                        fecha_emision, cantidad, precio_pago, estado_deuda):
+        return PaymentHistoryService.create_payment_history(idDepartamento, idGasto,
+                                                fecha_emision, cantidad, precio_pago, estado_deuda)
+            
+    @staticmethod
+    def get_payment_history_controller():        
+        return PaymentHistoryService.get_all_payment_histories()
+    
+    @staticmethod
+    def get_payment_history_by_id_controller(idDepartamento, idGasto):
+        return PaymentHistoryService.get_payment_history_by_id(idDepartamento, idGasto)
