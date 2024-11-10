798bcc681744afcded084fde531df6da08d1ab9f
Fabian Mella Barra <fa.mella@duocuc.cl>
2024-11-10 14:28:16 -0300

    Update department sevrice

---
+++ services/DepartmentService.py
@@ -0,0 +1,24 @@
+from models.DepartmentModel import db, Department
+
+class DepartmentService:
+    
+    @staticmethod
+    def create_department(id_departmento, n_piso, direccion, n_telefono, disponibilidad):
+        department = Department(
+            id_departmento=id_departmento,
+            n_piso=n_piso,
+            direccion=direccion,
+            n_telefono=n_telefono,
+            disponibilidad=disponibilidad
+        )
+        db.session.add(department)
+        db.session.commit()
+        return department 
+        
+    @staticmethod
+    def get_all_departments():
+        return Department.query.all()        
+    
+    @staticmethod
+    def get_department_by_id(department_id):
+        return Department.query.get(department_id)
