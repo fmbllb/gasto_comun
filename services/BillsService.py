551c0eeb9b0e5d223824ce9b2b395f48bc3cfbbf
Fabian Mella Barra <fa.mella@duocuc.cl>
2024-11-10 14:46:03 -0300

    Update bill service

---
+++ services/BillsService.py
@@ -0,0 +1,28 @@
+from models.BillModel import db, Bill
+
+class BillService:
+    
+    @staticmethod
+    def create_bill(id_gasto, nom_gasto, total_gasto, fecha_gasto, tipo_gasto):
+        bill = Bill(
+            id_gasto=id_gasto,
+            nom_gasto=nom_gasto,
+            total_gasto=total_gasto,
+            fecha_gasto=fecha_gasto,
+            tipo_gasto=tipo_gasto
+        )
+        db.session.add()
+        db.session.commit()
+        return bill 
+        
+    @staticmethod
+    def get_all_bills():
+        return Bill.query.all()        
+    
+    @staticmethod
+    def get_bill_by_id(id_departmento):
+        return Bill.query.get(id_departmento)
+    
+    @staticmethod
+    def get_bill_by_date(fecha_gasto):
+        return Bill.query.filter_by(fecha_gasto=fecha_gasto).all()
