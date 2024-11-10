be2473782dacb3cddeac2d1a320e5b7561fa51d7
Fabian Mella Barra <fa.mella@duocuc.cl>
2024-11-10 14:42:15 -0300

    Update department controller

--- controllers/DepartmentController.py
+++ controllers/DepartmentController.py
@@ -2,15 +2,17 @@ from services.DepartmentService import DepartmentService
 
 class DepartmentController:    
     @staticmethod
-    def create_department_controller(department_id, floor, address, phone_number, availability):        
-        return DepartmentService.create_user(
-            department_id, floor, address, 
-            phone_number, availability)
+    def create_department_controller(id_departamento, 
+        n_departamento, n_piso, direccion, n_telefono, 
+        disponibilidad):        
+        return DepartmentService.create_department(
+            id_departamento, n_departamento, n_piso, 
+            direccion, n_telefono, disponibilidad)
             
     @staticmethod
     def get_department_controller():        
         return DepartmentService.get_all_departments()
     
     @staticmethod
-    def get_department_by_id_controller(department_id):
-        return DepartmentService.get_department_by_id(department_id)
+    def get_department_by_id_controller(id_departamento):
+        return DepartmentService.get_department_by_id(id_departamento)
