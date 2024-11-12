64c5c33018a7725d55c5b7c9fd63a31776de82ea
Fabian Mella Barra <fa.mella@duocuc.cl>
2024-11-12 19:33:51 -0300

    Update payment history service
    Implementacion de una funcion para obtener departamentos con el estado de moroso

--- services/PaymentHistoryService.py
+++ services/PaymentHistoryService.py
@@ -1,15 +1,16 @@
-from models.PaymentHistoryModel import db, PaymentHistory
+from typing import Dict, List, Optional
+from models.PaymentHistoryModel import EstadoDeuda, db, PaymentHistory
 
 class PaymentHistoryService:
     
     @staticmethod
-    def create_payment_history(idDepartamento, idGasto, fecha_emision, cantidad, precio_pago, estado_deuda):
+    def create_payment_history(idDepartamento, idGasto, fecha_emision, cantidad, monto_pagado, estado_deuda):
         payment_history = PaymentHistory(
             idDepartamento=idDepartamento,
             idGasto=idGasto,
             fecha_emision=fecha_emision,
             cantidad=cantidad,
-            precio_pago=precio_pago,
+            monto_pagado=monto_pagado,
             estado_deuda=estado_deuda
         )
         db.session.add(payment_history)
@@ -23,3 +24,17 @@ class PaymentHistoryService:
     @staticmethod
     def get_payment_history_by_id(idDepartamento, idGasto):
         return PaymentHistory.query.get((idDepartamento, idGasto))
+
+    @staticmethod
+    def obtener_departamentos_morosos(limit: Optional[int] = None) -> List[Dict]:
+        query = PaymentHistory.query.filter_by(estado_deuda=EstadoDeuda.MOROSO)
+        
+        if limit:
+            query = query.limit(limit)
+            
+        morosos = query.all()
+        return [m.serialize() for m in morosos]
+    
+    @staticmethod
+    def get_payment_history_by_date(fecha_emision):
+        return PaymentHistory.query.filter_by(fecha_emision=fecha_emision).all()
