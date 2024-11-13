8fff75b345ae4734debfd56247f38e94f8e9eccd
Fabian Mella Barra <fa.mella@duocuc.cl>
2024-11-13 10:16:36 -0300

    Update department service
    Agregacion del metodo para registrar pago

--- services/DepartmentService.py
+++ services/DepartmentService.py
@@ -1,4 +1,5 @@
 from models.DepartmentModel import asociar_facturas_a_departamento, db, Department
+from models.PaymentHistoryModel import EstadoDeuda, PaymentHistory
 
 class DepartmentService:
     
@@ -34,3 +35,26 @@ class DepartmentService:
             return department
         else:
             return None
+    
+    @staticmethod
+    def registrar_pago(id_departamento, id_gasto, precio_pago, estado_deuda):
+        # Buscar la entrada en PaymentHistory
+        payment_history_entry = PaymentHistory.query.filter_by(
+            idDepartamento=id_departamento,
+            idGasto=id_gasto
+        ).first()
+        # Si el registro no existe, simplemente retornar None para que la vista maneje el error
+        if not payment_history_entry:
+            return None
+        # Actualizar el monto pagado
+        payment_history_entry.monto_pagado += precio_pago
+        # Cambiar el estado de la deuda si está completamente pagada
+        if payment_history_entry.monto_pagado >= payment_history_entry.precio_total:
+            payment_history_entry.estado_deuda = EstadoDeuda.PAGADO
+        else:
+            # Mantener el estado actual o actualizarlo a un estado parcial si es necesario
+            payment_history_entry.estado_deuda = estado_deuda
+        # Confirmar los cambios en la base de datos
+        db.session.commit()
+
+        return payment_history_entry  # Devolver el objeto actualizado para que la vista pueda utilizarlo si es necesario
